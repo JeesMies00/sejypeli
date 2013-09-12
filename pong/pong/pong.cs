1b9caefa3a64d4f4f3d6504d01994aa0ab27b4e1
JeesMies00 <www.atte.alalahti@gmail.com>
2013-09-12 08:14:03 -0700

    Tein hienon pong pelin.

---
+++ pong/pong/pong.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Jypeli;
+using Jypeli.Assets;
+using Jypeli.Controls;
+using Jypeli.Effects;
+using Jypeli.Widgets;
+
+public class pong : PhysicsGame
+{
+    PhysicsObject pallo;
+
+    public override void Begin()
+    {
+        LuoKentta();
+
+        AloitaPeli();
+
+        // TODO: Kirjoita ohjelmakoodisi tähän
+
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+    }
+
+    void LuoKentta()
+    {
+        pallo = new PhysicsObject(40, 40);
+        pallo.Shape = Shape.Circle;
+        pallo.Restitution = 1.0;
+        pallo.X = -200.0;
+        pallo.Y = 0.0;
+        Add(pallo);
+
+        LuoMaila(Level.Left + 20.0, 0.0);
+        LuoMaila(Level.Right - 20.0, 0.0);
+
+        Level.CreateBorders(1.0, false);
+
+        Level.Background.Color = Color.LimeGreen;
+
+        Camera.ZoomToLevel();
+    }
+    void AloitaPeli()
+    {
+        Vector impulssi = new Vector(500.0, 0.0);
+        pallo.Hit(impulssi);
+    }
+    void LuoMaila(double x, double y)
+    {
+        PhysicsObject maila = PhysicsObject.CreateStaticObject(20.0, 100.0);
+        maila.Shape = Shape.Rectangle;
+        maila.X = x;
+        maila.Y = y;
+        maila.Restitution = 1.0;
+        Add(maila);
+    }
+    void AsetaOhjaimet();
+
+}
