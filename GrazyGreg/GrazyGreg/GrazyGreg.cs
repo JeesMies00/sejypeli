3ccc04a362baf8668d651e6e9697510ac3c95c1a
JeesMies00 <www.atte.alalahti@gmail.com>
2014-03-13 18:29:17 +0200

    Gg

--- GrazyGreg/GrazyGreg/GrazyGreg.cs
+++ GrazyGreg/GrazyGreg/GrazyGreg.cs
@@ -8,7 +8,7 @@ using Jypeli.Widgets;
 
 public class GrazyGreg : PhysicsGame
 {
-    PhysicsObject Greg = new PhysicsObject(100, 140);
+    PhysicsObject Greg = new PhysicsObject(140, 140);
     
     
     public override void Begin()
@@ -16,22 +16,58 @@ public class GrazyGreg : PhysicsGame
         Gravity = new Vector(0.0, -5.0);
         Level.Background.CreateGradient(Color.GreenYellow, Color.Magenta);
         Mouse.IsCursorVisible = true;
+        Camera.ZoomToLevel(600.0);
+        //Camera.FollowedObject = Greg;
         
-        Image GreginKuva = LoadImage("Greg");
-        Greg.Image = GreginKuva;
-        Add(Greg);
-
         Mouse.Listen(MouseButton.Left, ButtonState.Pressed, Hyppy, "Hyppää");
         PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
     }
 
+    void LuoKentta()
+    {
+        ColorTileMap taso = ColorTileMap.FromLevelAsset("level1");
+        taso.SetTileMethod(Color.Black, LuoSeina);
+    }
+        
         void Hyppy()
         {
+            Vector hiirenPaikka = new Vector();
+            Vector hahmonPaikka = new Vector();
+            hiirenPaikka = Mouse.PositionOnWorld;
+            hahmonPaikka = Greg.AbsolutePosition;
+            Vector suunta = hiirenPaikka - hahmonPaikka;
 
-            Vector impulssi = new Vector();
-
+            Vector impulssi = Vector.FromLengthAndAngle(suunta.Magnitude, suunta.Angle + Angle.FromDegrees(180));
             Greg.Hit(impulssi);
         }
+        void LuoSeina(Vector paikka, double leveys, double korkeus)
+        {
+            PhysicsObject seina = PhysicsObject.CreateStaticObject(leveys, korkeus);
+            seina.Position = paikka;
+            Image seinanKuva = LoadImage("wall");
+            seina.Image = seinanKuva;
+            seina.CollisionIgnoreGroup = 1;
+            Add(seina);
+        }
+        void LuoPelaaja(Vector paikka, double leveys, double korkeus)
+        {
+            Greg.Position = paikka;
+            Image GreginKuva = LoadImage("Greg");
+            Greg.AngularVelocity = 2;
+            Greg.Image = GreginKuva;
+            Add(Greg);
+        }
+        void LuoLippu(Vector paikka, double leveys, double korkeus)
+        {
+            PhysicsObject ovi = PhysicsObject.CreateStaticObject(leveys, korkeus);
+            ovi.Position = paikka;
+            Image ovenKuva = LoadImage("door");
+            ovi.Image = ovenKuva;
+            ovi.CollisionIgnoreGroup = 1;
+            Add(ovi);
+        }
+
+
     }
 
